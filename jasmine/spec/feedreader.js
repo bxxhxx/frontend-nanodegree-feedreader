5479693d9e09ad76adc59482f778f659db8b7cb3
bxxhxx <Tom@New-Box-17-Edit.local>
2016-03-18 19:00:48 -0700

    complete readme file and full comments with feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,8 +33,11 @@ $(function() {
          */
         it('URL defined', function() {
             var arrayLength = allFeeds.length;
+            //Make a loop that goes through the whole array
             for (i = 0; i < arrayLength; i++) {
+                //It checks to make sure url's are defined
                 expect(allFeeds[i].url).toBeDefined();
+                //It checks to make sure the url is not empty
                 expect(allFeeds[i].url.length).not.toBe(0);
             }
         });
@@ -46,8 +49,11 @@ $(function() {
          */
         it('name defined', function() {
             var nameArray = allFeeds.length;
+            //Make a loop that goes through each name array
             for (i = 0; i < nameArray; i++) {
+                //it checks to make sure the name array is defined
                 expect(allFeeds[i].name).toBeDefined();
+                //it checks to make sure the name array is not empty
                 expect(allFeeds[i].name.length).not.toBe(0);
             }
         });
@@ -63,7 +69,11 @@ $(function() {
          * hiding/showing of the menu element.
          */
         it('are hidden', function() {
+            /* "body" is the element that recieves the menu-hidden class
+             * menu-hidden triggers the slide menu to be off screen via css
+             */
             var element = $('body');
+            //checks to see if body has recieved the menu-hidden class after load
             expect(element.hasClass('menu-hidden')).toBe(true);
         });
 
@@ -75,9 +85,15 @@ $(function() {
         it('ensures the menu changes visibility when the menu icon is clicked', function() {
             var element = $('body');
 
+            /* using jquery to trigger click on menu-icon. And then test to see
+             * if the menu-hidden class is removed
+             */
             $('.menu-icon-link').trigger('click');
             expect(element.hasClass('menu-hidden')).toBe(false);
 
+            /* using jquery to trigger another click on menu-icon. And then test to see
+             * if the menu-hidden class is restored
+             */
             $('.menu-icon-link').trigger('click');
             expect(element.hasClass('menu-hidden')).toBe(true);
         });
@@ -92,10 +108,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         /* using beforEach method to run loadFeed prior to test
+          * and usind the done function to delay the test until after
+          * asynchronous loadFeed is completed. done is used as a callback.
+          */
         beforeEach(function(done) {
             loadFeed(1, done);
         });
 
+        /* using jquery to gather all of the entries after loadFeed is run.
+         * measure the length and then the test checks whether the length
+         * is greater than zero, meaning there is at least one element
+         */
         it('should have at least one .entry element after loadFeed is called', function(done) {
             var entryLength = $('.entry').length;
             expect(entryLength).toBeGreaterThan(0);
@@ -112,12 +136,19 @@ $(function() {
          */
         var topHeader;
 
+        /* beforeEach loads one feed, then stores the content of the first entry header
+         * in the variable topHeader. Next it loads a different feed and completes by running
+         * the done callback which allows the test to run
+         */
         beforeEach(function(done) {
             loadFeed(0);
             topHeader = $('.entry h2:first').text();
             loadFeed(2, done);
         });
 
+        /* it test to make sure the content of the newTopHeader is not the same as
+         * the previous header
+         */
         it('entry content should change', function(done) {
             var newTopHeader = $('.entry h2:first').text();
             expect(newTopHeader).not.toMatch(topHeader);
